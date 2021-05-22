c3c643fa3003ff04d4c2e7d63a975265cc462524
Alicia Edwards <aliciaedwardsdesign@gmail.com>
2021-05-22 19:20:18 -0400

    initial code

---
+++ MadLibs.js
@@ -0,0 +1,29 @@
+let questions = 3;
+
+let questionsLeft = ' [' + questions + ' questions left]';
+
+let adjective = prompt('Please type an adjective' + questionsLeft);
+
+questions -= 1;
+
+questionsLeft = ' [' + questions + ' questions left]';
+
+let verb = prompt('Please type a verb that does not end in -ing ' + questionsLeft);
+
+questions -= 1;
+
+questionsLeft = ' [' + questions + ' questions left]';
+
+let noun = prompt('Please type a noun' + questionsLeft);
+
+alert('All done. Ready for the message?');
+
+let sentence = "<p>There was a " + adjective;
+
+sentence += ' UX/UI designer who wanted to learn Front-End Development to ' + verb;
+
+sentence += ' the ' + noun + '.</p>';
+
+document.write(sentence);
+//document.getElementById('results').innerHTML = sentence;
+//document.createElement(sentence);
